e2d60f148a7f2b74067643768e4a66352e1cc3da
Faber Silitonga <abrosua@gmail.com>
2018-12-18 18:37:28 +0700

    minor changes only, can be neglected

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import gridgen_fcn as gg
 import numpy as np
 import matplotlib.pyplot as plt
 
-filename = "2410.txt"
+filename = "4412.txt"
 
 mid_panel, normal_panel, length_panel, airfoil = ia.importer(filename)
+tes = np.linalg.norm(normal_panel, axis=1)
 
 # input grid
 domsize = 2
@@ -14,11 +15,11 @@ npoints = 10
 grid = gg.pointsgen(filename, domsize, npoints)
 
 # input boundary condition
-u_inf = np.array([5, 0])
+u_inf = np.array([1, 0])
 g = 0
 
 # calculate potential
-layer = 'single'
+layer = 'double'
 pot_panel, pot_grid, c_p, c_l = pl.pot(mid_panel, normal_panel, length_panel, g, u_inf, layer, grid)
 
 # post-processing
