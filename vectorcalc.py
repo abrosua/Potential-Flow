11178ae5966970f7904e331877ca4feec7b225d1
Faber Silitonga <abrosua@gmail.com>
2018-12-18 18:40:19 +0700

    calculation for rho and rho+n

--- vectorcalc.py
+++ vectorcalc.py
@@ -8,17 +8,17 @@ from copy import deepcopy
 
 def calc_r(mid_panel, normal_panel):
     # New Version, distance of a panel with respect to other panels
-    r_x = np.tensordot(mid_panel[:, 0], np.ones(len(mid_panel)), axes=0);
+    r_x = np.tensordot(mid_panel[:, 0], np.ones(len(mid_panel)), axes=0)
     r_xdist = deepcopy(r_x)
-    r_y = np.tensordot(mid_panel[:, 1], np.ones(len(mid_panel)), axes=0);
+    r_y = np.tensordot(mid_panel[:, 1], np.ones(len(mid_panel)), axes=0)
     r_ydist = deepcopy(r_y)
     r_xdist[:, :] = r_xdist - np.transpose(r_x)
     r_ydist[:, :] = r_ydist - np.transpose(r_y)
     r = np.sqrt(r_xdist ** 2 + r_ydist ** 2)
 
     # calculate magnitude r+r_n
-    rn_xdist = np.tensordot(normal_panel[:, 0], np.ones(len(normal_panel)), axes=0)
-    rn_ydist = np.tensordot(normal_panel[:, 1], np.ones(len(normal_panel)), axes=0)
+    rn_xdist = np.tensordot(np.ones(len(normal_panel)), normal_panel[:, 0], axes=0)
+    rn_ydist = np.tensordot(np.ones(len(normal_panel)), normal_panel[:, 1], axes=0)
     rn_xdist[:, :] = r_xdist + rn_xdist
     np.fill_diagonal(rn_xdist, 0)
     rn_ydist[:, :] = r_ydist + rn_ydist
@@ -28,15 +28,21 @@ def calc_r(mid_panel, normal_panel):
     return r, rplusrn
 
 
-def calc_rho(grid, normal_panel):
+def calc_rho(grid, mid_panel, normal_panel):
     # New version recreate grid coordinate as an 2-D array
-    rho_xgrid = np.tensordot(grid[:, 0], np.ones(len(normal_panel)), axes=0)
-    rho_ygrid = np.tensordot(grid[:, 1], np.ones(len(normal_panel)), axes=0)
+    rho_xgrid = np.tensordot(grid[:, 0], np.ones(len(mid_panel)), axes=0)
+    rho_ygrid = np.tensordot(grid[:, 1], np.ones(len(mid_panel)), axes=0)
+    rho_xpan = np.tensordot(np.ones(len(grid)), mid_panel[:, 0], axes=0)
+    rho_ypan = np.tensordot(np.ones(len(grid)), mid_panel[:, 1], axes=0)
+    rho_xdist = rho_xgrid - rho_xpan
+    rho_ydist = rho_ygrid - rho_ypan
+    rho = np.sqrt(rho_xdist**2 + rho_ydist**2)
+
+    # Calculate magnitude of rho + rho_n
     rho_xfoil = np.tensordot(np.ones(len(grid)), normal_panel[:, 0], axes=0)
     rho_yfoil = np.tensordot(np.ones(len(grid)), normal_panel[:, 1], axes=0)
-    rho_x = rho_xgrid - rho_xfoil
-    rho_y = rho_ygrid - rho_yfoil
-    rho = np.sqrt(rho_x ** 2 + rho_y ** 2)
-    rho_n = np.NAN  # Still have no ideas bout this
+    rho_x = rho_xdist + rho_xfoil
+    rho_y = rho_ydist + rho_yfoil
+    rho_n = np.sqrt(rho_x ** 2 + rho_y ** 2)
 
     return rho, rho_n
